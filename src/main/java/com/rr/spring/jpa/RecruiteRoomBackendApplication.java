7f5c58dc20102e88885a297a71a1ce49bf65ea03
Manav Sharma <mshar246@my.centennialcollege.ca>
2025-03-05 02:39:02 -0500

    main application method updated

--- src/main/java/com/rr/spring/jpa/RecruiteRoomBackendApplication.java
+++ src/main/java/com/rr/spring/jpa/RecruiteRoomBackendApplication.java
@@ -2,14 +2,28 @@ package com.rr.spring.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(
+		scanBasePackages= {"com.recruitrooms.config",
+				"com.recruitrooms.controllers",
+				"com.recruitrooms.dto",
+				"com.recruitrooms.models",
+				"com.recruitrooms.repositories",
+				"com.recruitrooms.services",
+				"com.recruitrooms.utils",
+				"com.rr.spring.jpa"
+		})
+@ComponentScan("com.rr.spring.jpa")
+@EntityScan(basePackages = {"com.recruitrooms.services"})
 
 
-@SpringBootApplication
 public class RecruiteRoomBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecruiteRoomBackendApplication.class, args);
-		
+		System.out.println("Running springboot app for book..");		
 	}
-
 }
+
